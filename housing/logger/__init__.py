f5120f0062767e871d515c2c9e1d3f5b56c08988
GodWin <shivamjpanchal1@gmail.com>
2022-08-09 16:59:19 +0530

    refactor(.gitignore/logger): artifact added in .gitignore & changed logger format

--- housing/logger/__init__.py
+++ housing/logger/__init__.py
@@ -12,6 +12,6 @@ LOG_FILE_PATH = os.path.join(LOG_DIR, LOG_FILE_NAME)
 logging.basicConfig(
     filename=LOG_FILE_PATH,
     filemode="w",
-    format="[%(asctime)s] %(levelname)s: %(name)s:%(module)s => %(message)s",
+    format="[%(asctime)s] %(levelname)s: %(name)s:%(module)s:%(filename)s:%(funcName)s() => %(message)s",
     level=logging.INFO,
 )
