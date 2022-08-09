b7a81ab933d64dea44094670b9b47f4268fcb7b1
GodWin <shivamjpanchal1@gmail.com>
2022-08-09 13:05:05 +0530

    refactor(comp/data_ingestion): logger msg updated

--- housing/component/data_ingestion.py
+++ housing/component/data_ingestion.py
@@ -6,7 +6,7 @@ from housing.logger import logging
 class DataIngestion:
     def __init__(self, data_ingestion_config: DataIngestionConfig):
         try:
-            logging.info(f"Data Ingestion Log Started".center(60))
+            logging.info(f"Data Ingestion Log Started".center(60, "-"))
             self.data_ingestion_config = data_ingestion_config
         except Exception as e:
             raise HousingException(e) from e
